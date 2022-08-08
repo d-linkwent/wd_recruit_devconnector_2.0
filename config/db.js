208e6c6df6850cb19dace93df44ec5ce16be59d0
David Taylor <bspowk@gmail.com>
2022-08-08 10:19:06 +0100

    mongoose connect update

--- config/db.js
+++ config/db.js
@@ -4,11 +4,11 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {});
-
-    console.log('MongoDB Connected...');
-  } catch (err) {
-    console.error(err.message);
+    await mongoose.connect(
+      'mongodb+srv://MernAdmin:MernAdmin1@clustermerndtone.8ghkwoo.mongodb.net/?retryWrites=true&w=majority'
+    );
+  } catch (error) {
+    handleError(error);
     // Exit process with failure
     process.exit(1);
   }
